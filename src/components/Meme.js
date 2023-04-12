0aefba143ad2500e840976eb636b43b510f3af01
Carter Clifford <carterlc@live.ca>
2023-04-12 00:01:32 -0600

    Added Ids to each input for future purposes. Also styled both inputs and button.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -5,8 +5,8 @@ export default function Meme() {
         <main>
             <form className='form'>
                 <button className='form--button'>Generate a New Meme</button>
-                <input type="text" placeholder='Top Text' />
-                <input type="text" placeholder='Bottom Text' />
+                <input type="text" placeholder='Top Text' id='top-text' />
+                <input type="text" placeholder='Bottom Text' id='bottom-text' />
             </form>
         </main>
     )
