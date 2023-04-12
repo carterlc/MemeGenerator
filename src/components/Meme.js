de9c50457a88fc8b600a1d5e5c53d5b297616784
Carter Clifford <carterlc@live.ca>
2023-04-12 00:17:23 -0600

    Added a meme data array and a function to the generate meme button.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,16 @@
 import React from 'react'
 
 export default function Meme() {
+function getMeme() {
+    console.log('hello');
+}
     return (
         <main>
-            <form className='form'>
-                <button className='form--button'>Generate a New Meme</button>
+            <div className='form'>
+                <button className='form--button' onClick={getMeme}>Generate a New Meme</button>
                 <input type="text" placeholder='Top Text' id='top-text' />
                 <input type="text" placeholder='Bottom Text' id='bottom-text' />
-            </form>
+            </div>
         </main>
     )
 }
