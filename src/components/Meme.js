c7c40a8db16c148818e6437c1e8adff723d65ae4
Carter Clifford <carterlc@live.ca>
2023-04-12 02:16:06 -0600

    Added a useState to the Meme.js component in which sets the state of memeImage with a random meme every time the getMeme function is called.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import memeData from '../memeData';
 
 export default function Meme() {
 
-function getMeme() {
-    const memesArray = memeData.data.memes
-    const randomNumber = Math.floor(Math.random() * memesArray.length)
-    const url = memesArray[randomNumber].url
-    console.log(url)
-}
+    const [memeImage, setMemeImage] = useState('')
+
+    function getMeme() {
+        const memesArray = memeData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
     return (
         <main>
             <div className='form'>
                 <button className='form--button' onClick={getMeme}>Generate a New Meme</button>
                 <input type="text" placeholder='Top Text' id='top-text' />
+                <img src={memeImage} className='meme--image'/>
                 <input type="text" placeholder='Bottom Text' id='bottom-text' />
             </div>
         </main>
