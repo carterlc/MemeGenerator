5a52121076a7fe8b275f4d364057369b05ddb83e
Carter Clifford <carterlc@live.ca>
2023-04-13 00:04:27 -0600

    Refactor meme generator component to use React Hooks.
    useState is used to initialize and manage state, and the getMeme function is updated to use the allMemeImages state instead of importing memeData.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -3,19 +3,31 @@ import memeData from '../memeData';
 
 export default function Meme() {
 
-    const [memeImage, setMemeImage] = useState('')
+    const [meme, setMeme] = useState({
+        topText: '',
+        bottomText: '',
+        randomImage: 'http://i.imgflip.com/1bij.jpg'
+    })
+
+    const [allMemeImages, setAllMemeImages] = useState(memeData);
 
     function getMeme() {
-        const memesArray = memeData.data.memes
+        const memesArray = allMemeImages.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url = memesArray[randomNumber].url
+        setMeme(prevState => {
+            return {
+                ...prevState,
+                randomImage: url
+            }
+        })
     }
     return (
         <main>
             <div className='form'>
                 <button className='form--button' onClick={getMeme}>Generate a New Meme</button>
                 <input type="text" placeholder='Top Text' id='top-text' />
-                <img src={memeImage} className='meme--image'/>
+                <img src={meme.randomImage} className='meme--image' />
                 <input type="text" placeholder='Bottom Text' id='bottom-text' />
             </div>
         </main>
