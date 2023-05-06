efd23099e769dfc0ede7d3eaab4984e0bee7e0ae
Carter Clifford <carterlc@live.ca>
2023-05-06 15:07:27 -0600

    Added two h2 to the top and bottom of the meme image. Input data is saved through state.

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -22,13 +22,26 @@ export default function Meme() {
             }
         })
     }
+
+    function handleChange(event) {
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
     return (
         <main>
             <div className='form'>
                 <button className='form--button' onClick={getMeme}>Generate a New Meme</button>
-                <input type="text" placeholder='Top Text' id='top-text' />
-                <img src={meme.randomImage} className='meme--image' />
-                <input type="text" placeholder='Bottom Text' id='bottom-text' />
+                <input type="text" placeholder='Top Text' id='top-text' name='topText' value={meme.topText} onChange={handleChange}/>
+                <div className="meme">
+                    <h2 className="meme--top text">{meme.topText}</h2>
+                    <h2 className="meme--bottom text">{meme.bottomText}</h2>
+                    <img src={meme.randomImage} className='meme--image' alt="meme"/>
+                </div>
+                <input type="text" placeholder='Bottom Text' id='bottom-text' name='bottomText' value={meme.bottomText} onChange={handleChange}/>
             </div>
         </main>
     )
